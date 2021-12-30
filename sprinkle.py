000b1aaeb58a689ebc89a2737f34007ed974e88d
KazaKazan <ahmetfiratusta@hotmail.com>
2021-12-30 14:22:41 +0300

    Add logic to create tile sheet in compact mode.

--- sprinkle.py
+++ sprinkle.py
@@ -23,12 +23,12 @@ def getImages():
                 pass
         if images != []:
             if verbose:
-                print("Found " + str(len(images)) + " image files in the current directory.")
+                print("     Found " + str(len(images)) + " image files in the current directory.\n")
             return images
         else:
-            sys.exit("No image files found!")
+            sys.exit("      No image files found!\n")
     else:
-        sys.exit("No image files found!")
+        sys.exit("      No image files found!\n")
 
 def calculateSetSize(imageFiles, spriteSize = 32, compactMode = True):
     if verbose:
@@ -40,9 +40,61 @@ def calculateSetSize(imageFiles, spriteSize = 32, compactMode = True):
             with Image.open(image) as im:
                 tileCount += math.ceil(im.size[0] / spriteSize)
                 tileCount += math.ceil(im.size[1] / spriteSize)
-        sheetSize = math.ceil(math.sqrt(tileCount)) * spriteSize
+        sheetSize = math.ceil(math.sqrt(tileCount))
     else:
         pass
     if verbose:
-        print("Sprite sheet size calculated as " + str(sheetSize) + "x" + str(sheetSize))
+        print("     Sprite sheet size calculated as " + str(sheetSize) + "x" + str(sheetSize) + "\n")
     return sheetSize
+
+def setRowColumn(cRow, cColumn, size):
+    row = cRow
+    column = cColumn
+    if cRow + 1 == size:
+        row = 0
+        column += 1
+    else:
+        row += 1
+    return row, column
+
+def createSheet(imageFiles, size, spriteSize = 32, compactMode = True):
+    if verbose:
+        print("Creating sprite sheet.")
+    sheet = Image.new("RGBA", (size * spriteSize, size * spriteSize))
+    if verbose:
+        print("     Placing sprites onto the sprite sheet.")
+    if compactMode:
+        row = 0
+        column = 0
+        for i in range(len(imageFiles)):
+            with Image.open(imageFiles[i]) as im:
+                if im.size[0] > spriteSize or im.size[1] > spriteSize:
+                    rowTotal = math.ceil(im.size[0] / spriteSize)
+                    columnSection = 0
+                    columnTotal = math.ceil(im.size[1] / spriteSize)
+                    columnEnd = spriteSize
+                    while columnSection < columnTotal:
+                        rowSection = 0
+                        rowEnd = spriteSize
+                        while rowSection < rowTotal:
+                            box = (rowSection * spriteSize, columnSection * spriteSize, rowEnd, columnEnd)
+                            section = im.crop(box)
+                            rowSection += 1
+                            rowEnd = (rowSection + 1) * spriteSize
+                            if rowEnd > im.size[0]:
+                                rowEnd = im.size[0]
+                            if len(section.getcolors()) == 1 and section.getcolors()[0][1] == (0, 0, 0, 0):
+                                pass
+                            else:
+                                sheet.paste(section, (spriteSize * row, spriteSize * column))
+                            row, column = setRowColumn(row, column, size)
+                        columnSection += 1
+                        columnEnd = (columnSection + 1) * spriteSize
+                        if columnEnd < im.size[1]:
+                            columnEnd = im.size[1]
+                else:
+                    sheet.paste(im, (spriteSize * row, spriteSize * column))
+                    row, column = setRowColumn(row, column, size)
+    else:
+        pass
+    sheet.show()
