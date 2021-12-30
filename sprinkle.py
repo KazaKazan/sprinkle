1c7a70a8793a9f4883722905a5b303317b1af221
KazaKazan <ahmetfiratusta@hotmail.com>
2021-12-30 11:51:34 +0300

    Add .gitignore and add function to get image files in the current working directory.

--- sprinkle.py
+++ sprinkle.py
@@ -2,3 +2,24 @@
 
 import sys, os
 from PIL import Image
+
+def getImages():
+    fileList = os.listdir(os.path.abspath(os.getcwd()))
+    files = []
+    for file in fileList:
+        if os.path.isfile(file):
+            files.append(file)
+    if files != []:
+        images = []
+        for file in files:
+            try:
+                with Image.open(file) as im:
+                    images.append(file)
+            except OSError:
+                pass
+        if images != []:
+            return images
+        else:
+            print("No image files found!")
+    else:
+        print("No image files found!")
